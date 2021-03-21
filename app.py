4d0a1768acfecc7e4ca5876f119ffb92c8758c2d
Cldwlkn13 <gavin_rudge@hotmail.co.uk>
2021-03-21 14:53:43 +0000

    added login functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -27,8 +27,53 @@ def get_recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(
+                request.form.get("password")),
+            "email": request.form.get("email"),
+            "first_name": request.form.get("first_name").lower(),
+            "last_name": request.form.get("last_name").lower(),
+            "dob": request.form.get("dob"),
+            "country": request.form.get("country").lower()
+        }
+        mongo.db.users.insert_one(register)
+
+        session['user'] = request.form.get("username").lower()
+        flash('Regisration Succcessful')
+
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        existing_user = mongo.db.users.find_one(
+            {"username" : request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user['password'], request.form.get("password")):
+                    session['user'] = request.form.get("username").lower()
+                    flash("Welcome back, {}".format(request.form.get("username")))
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for('login'))
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for('login'))
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,48 @@
-  $(document).ready(function(){
-    $(".sidenav").sidenav({edge: "right"});
+$(document).ready(function () {
+    $(".sidenav").sidenav({ edge: "right" });
 
-    $.get('static/text/countries.txt',function(data) {
+    $.get('static/text/countries.txt', function (data) {
         var lines = data.split('\n');
-            $.each(lines, function(k, v){
-                var $newOpt = $("<option>").attr("value",v).text(v)
-                $('#country').append($newOpt);
-            });
-      });
+        $.each(lines, function (k, v) {
+            var $newOpt = $("<option>").attr("value", v).text(v)
+            $('#country').append($newOpt);
+        });
+    });
 
     $('select').formSelect();
-  });
+
+    $("#password_confirm").on("focusout", function (e) {
+        if (!validatePassword()) {
+            $(this).removeClass("valid").addClass("invalid");
+            $("#lbl_password_confirm").text("Passwords do not match")
+        } else {
+            $(this).removeClass("invalid").addClass("valid");
+            $("#lbl_password_confirm").text("Passwords match")
+        }
+    });
+
+    $('#register-form').submit(function (e) {
+        var bool = validatePassword();
+        if (!bool){
+            e.preventDefault();
+            alert("Passwords do not match");
+        }
+        else
+            return true; //continue to submit form
+    });
+
+    function validatePassword() {
+        var password = $("#password").val();
+        var confirmPassword = $("#password_confirm").val();
+        if (password != confirmPassword) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    setTimeout(function() {
+        $('.flashes').fadeOut('slow');
+    }, 2000);
+});
